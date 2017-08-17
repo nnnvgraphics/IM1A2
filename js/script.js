8720b315c604e562c77d97f74f120575f161a1e6
nnnvgraphics <nnnvgraphics@gmail.com>
2017-08-17 09:48:07 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,61 @@
+$(function(){
+  'use strict';
+  var $page = $('#main'),
+      options = {
+        debug: true,
+        prefetch: true,
+        cacheLength: 5,
+        onStart: {
+          duration: 250, // Duration of our animation
+          render: function ($container) {
+            // Add your CSS animation reversing class
+            $container.addClass('is-exiting');
+            // Restart your animation
+            smoothState.restartCSSAnimations();
+          }
+        },
+        onReady: {
+          duration: 1000,
+          render: function ($container, $newContent) {
+            // Remove your CSS animation reversing class
+            $container.removeClass('is-exiting');
+            // Inject the new content
+            $container.html($newContent);
+          }
+        }
+      },
+      smoothState = $page.smoothState(options).data('smoothState');
+});
+
+/*
+$(function(){
+  'use strict';
+  var $page = $('#main'),
+      options = {
+        debug: true,
+        prefetch: true,
+        cacheLength: 2,
+        forms: 'form',
+        onStart: {
+          duration: 1000, // Duration of our animation
+          render: function ($container) {
+            // Add your CSS animation reversing class
+            $container.addClass('is-exiting');
+            // Restart your animation
+            smoothState.restartCSSAnimations();
+          }
+        },
+        onReady: {
+          duration: 0,
+          render: function ($container, $newContent) {
+            // Remove your CSS animation reversing class
+            $container.removeClass('is-exiting');
+            // Inject the new content
+            $container.html($newContent);
+          }
+        }
+      },
+      smoothState = $page.smoothState(options).data('smoothState');
+
+});
+*/
